75e4bdac8bab3ca7fdc707cf62d40da68f675287
yukimamura <sn037use@gmail.com>
2019-06-23 08:37:50 +0900

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { CommentDatePipe } from './pipe/comment-date.pipe';
@@ -14,7 +17,10 @@ import { CommentDatePipe } from './pipe/comment-date.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCQbbf6-7aAYPEhDS3_Ost6tE2hE6FPX78",
+    authDomain: "angular-chat-cheap.firebaseapp.com",
+    databaseURL: "https://angular-chat-cheap.firebaseio.com",
+    projectId: "angular-chat-cheap",
+    storageBucket: "",
+    messagingSenderId: "783889555771",
+    appId: "1:783889555771:web:41f0861fb9920fae" 
+  }
 };
 
 /*
