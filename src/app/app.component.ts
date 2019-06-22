8b3e0f9c885f10032515592d52f01a4bac551d7c
yukimamura <sn037use@gmail.com>
2019-06-23 07:35:17 +0900

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { Comment } from './class/comment';
+import { User } from './class/user';
+
+const CURRENT_USER: User = new User(1, '五十川 洋平');
+const ANOTHER_USER: User = new User(2, '竹井 賢治');
 
 const COMMENTS: Comment[] = [
-  { name: '竹井 賢治', message: 'お疲れ様です！' },
-  { name: '竹井 賢治', message: 'この前の件ですが、どうなりましたか？' },
-  { name: '五十川 洋平', message: 'お疲れ様です！' },
+  new Comment(ANOTHER_USER, 'お疲れ様です！'),
+  new Comment(ANOTHER_USER, 'この間の件ですが、どうなりましたか？'),
+  new Comment(CURRENT_USER, 'お疲れ様です！'),
+  new Comment(CURRENT_USER, 'クライアントからOKが出ました！'),
 ];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,4 +19,5 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
   comments = COMMENTS;
+  currentUser = CURRENT_USER;
 }
