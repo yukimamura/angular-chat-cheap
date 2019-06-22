1eb238ac0dbea2a13e2b114f4572864ff262b8e8
yukimamura <sn037use@gmail.com>
2019-06-23 07:12:10 +0900

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Comment } from './class/comment';
 
+const COMMENTS: Comment[] = [
+  { name: '竹井 賢治', message: 'お疲れ様です！' },
+  { name: '竹井 賢治', message: 'この前の件ですが、どうなりましたか？' },
+  { name: '五十川 洋平', message: 'お疲れ様です！' },
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat-cheap';
+  comments = COMMENTS;
 }
