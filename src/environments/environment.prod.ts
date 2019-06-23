62c662e2658cf5a41cb82ab4fc57cbd6e6fb7975
yukimamura <sn037use@gmail.com>
2019-06-23 14:48:48 +0900

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCQbbf6-7aAYPEhDS3_Ost6tE2hE6FPX78",
+    authDomain: "angular-chat-cheap.firebaseapp.com",
+    databaseURL: "https://angular-chat-cheap.firebaseio.com",
+    projectId: "angular-chat-cheap",
+    storageBucket: "",
+    messagingSenderId: "783889555771",
+    appId: "1:783889555771:web:41f0861fb9920fae" 
+  }
 };
