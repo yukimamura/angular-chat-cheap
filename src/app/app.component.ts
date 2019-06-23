c621eb84c678b1a49c0fbede2bf6050bad42c60c
yukimamura <sn037use@gmail.com>
2019-06-23 20:50:10 +0900

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ const CURRENT_USER: User = new User(1, '五十川 洋平');
 const ANOTHER_USER: User = new User(2, '竹井 賢治');
 
 const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'お疲れ様です！'),
+  new Comment(ANOTHER_USER, '本日は、誠にお疲れ様でございます！！'),
   new Comment(ANOTHER_USER, 'この間の件ですが、どうなりましたか？'),
   new Comment(CURRENT_USER, 'お疲れ様です！'),
   new Comment(CURRENT_USER, 'クライアントからOKが出ました！'),
